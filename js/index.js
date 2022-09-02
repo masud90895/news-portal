fe8d1e7cbadc2f86f8b4137490dea1c66157a530
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-09-02 21:55:22 +0600

    category section added

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// loadCategory 
+const loadCategory =async() =>{
+    const url ="https://openapi.programming-hero.com/api/news/categories"
+   try{
+    const res =await fetch (url)
+    const data = await res.json()
+    displayCategory(data.data.news_category)
+   }
+   catch(error){
+    console.log(error);
+   }   
+}
+
+
+const displayCategory= async (data)=>{
+    data.forEach(category => {
+        const {category_name} =category;
+        const categoryContainer =document.getElementById('catagory-container');
+        const categoriesDiv =document.createElement('li');
+        categoriesDiv.innerHTML=`
+        <a>${category_name}</a>
+        `;
+        categoryContainer.appendChild(categoriesDiv)
+        
+    });
+}
+
+
+// loadCategory call 
+loadCategory()
