58706061cf20b9a4472ce5b5547db75e3d3dae54
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-09-02 23:58:15 +0600

    card js added

--- js/index.js
+++ js/index.js
@@ -13,9 +13,9 @@ const loadCategory =async() =>{
 
 
 const displayCategory= async (data)=>{
+    const categoryContainer =document.getElementById('catagory-container');
     data.forEach(category => {
         const {category_name} =category;
-        const categoryContainer =document.getElementById('catagory-container');
         const categoriesDiv =document.createElement('li');
         categoriesDiv.classList.add("font-semibold");
         categoriesDiv.innerHTML=`
@@ -42,8 +42,28 @@ const loadCard =async() =>{
 
 
 const displayCard =(cards) =>{
+    console.log(cards);
+    const cardSection = document.getElementById('card-section');
     cards.forEach(card => {
-        console.log(card);
+        const {image_url,thumbnail_url,title,details,author,total_view,} = card;
+        const {name,published_date,img} =author;
+
+        // card section html 
+
+        const cardSectionDiv =document.createElement("div");
+        cardSectionDiv.classList.add("card", "card-side", "bg-base-100", "shadow-xl", "p-4", "mb-5")
+        cardSectionDiv.innerHTML =`
+                    <figure class="w-1/4"><img src="${thumbnail_url}" alt="Movie"></figure>
+                    <div class="card-body w-3/4">
+                      <h2 class="card-title">${title}</h2>
+                      <p>${details.length > 400 ? details.slice(0,400) + " ....." : details}</p>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                      </div>
+                    </div>
+                  
+        `
+        cardSection.appendChild(cardSectionDiv)
         
     });
 }
